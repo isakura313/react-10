22dbde38c7f72253854576705345132b25e2bfe2
doshen.vv <vasilisa_chehova@mail.ru>
2021-12-23 21:14:12 +0300

    slider

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
-import './Home.sass'
+import './Home.sass';
+import Carusel from '../Carusel';
 
 function Home() {
     return (
         <div className='pt-20 text-center h-screen'>
             <h2 className='text-2xl'> Добро пожаловать</h2>
             <h5> Сервис для статей и просмотра картинок</h5>
+            <Carusel className="h-400" />
         </div >
     )
 }
