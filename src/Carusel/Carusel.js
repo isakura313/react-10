74cc051d9ddd934ac1de6d369f6912efb42df260
Pavel Yakupov <pavel.yakupov@software-kit.ru>
2021-12-24 14:51:39 +0300

    add some deploy ficha slider and table to edit

--- src/Carusel/Carusel.js
+++ src/Carusel/Carusel.js
@@ -1,21 +1,24 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-// import "./Carusel.css"
+import "./carusel.sass";
 
 function Carusel() {
         return (
-            <Carousel className="h-400">
-                <div className="h-400">
-                    <img classname="min-h-0" src="https://images.pexels.com/photos/6210931/pexels-photo-6210931.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-                    
+            <Carousel className="h-20">
+                <div className="h-20">
+                    <div className="img_wrap" style={{backgroundImage: `url(https://images.pexels.com/photos/6210931/pexels-photo-6210931.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)`}}>
+                    {/* <img className="min-h-0" src="https://images.pexels.com/photos/6210931/pexels-photo-6210931.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" /> */}
+                    </div>
                 </div>
                 <div>
+                    <div className="img_wrap">   
                     <img src="https://images.pexels.com/photos/6087668/pexels-photo-6087668.jpeg?cs=srgb&dl=pexels-koolshooters-6087668.jpg&fm=jpg" />
-            
+                    </div>
                 </div>
                 <div>
+                    <div className="img_wrap">
                     <img src="https://images.pexels.com/photos/6102369/pexels-photo-6102369.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-                
+                    </div>
                 </div>
             </Carousel>
         );
